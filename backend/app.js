180cb2d3ba58385b52f39f85bfc9632df0332bdb
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2023-04-30 13:40:09 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:cIFMpgCUGTC113sI@cluster0.rb0ud7z.mongodb.net/test"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
